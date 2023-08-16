1d3319b6f415484dd1467dd4545c127afb419cc7
wwwalohacampus <h850415@naver.com>
2023-08-16 17:57:29 +0900

    🧡🧡

--- JAVA/src/Day08/Ex04_AnonymousObject/AnonymousPerson.java
+++ JAVA/src/Day08/Ex04_AnonymousObject/AnonymousPerson.java
@@ -13,7 +13,6 @@ public class AnonymousPerson {
 	};
 	
 	Person person2 = new Person() {
-		
 		void work() {
 			System.out.println(name + "(" + age + ")");
 			System.out.println("요리를 합니다.");
@@ -25,7 +24,7 @@ public class AnonymousPerson {
 		person.name = "김조은";
 		person.age = 20;
 		person.work();
-		
+
 		person2.name = "백종원";
 		person2.age = 40;
 		person2.work();
